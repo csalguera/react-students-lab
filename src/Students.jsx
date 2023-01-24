fe59a62bf2c07de905d3c1d0066c6fb92deec8e1
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-24 14:46:14 -0500

    add button elements and set up handleClick function to render each student

--- src/Students.jsx
+++ src/Students.jsx
@@ -5,9 +5,23 @@ const Students = () => {
 
   const [index, setIndex] = useState(0)
   let student = studentList[index]
+
+
+  function handleClick(evt) {
+    let content = evt.target.textContent
+    content === 'Next' ? setIndex(index + 1) : setIndex(index - 1)
+  }
+
   return (
     <>
+      <button onClick={handleClick}>
+        Next
+      </button>
+      <button onClick={handleClick}>
+        Previous
+      </button>
       <h1>{student.name}</h1>
+      <h2>{student.bio}</h2>
     </>
   )
 }
