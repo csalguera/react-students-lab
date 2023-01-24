b0e7017baa723d974c1a53a9fca42cb7372de109
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-24 16:43:16 -0500

    add function to show/hide bio and variable to persist that state

--- src/Students.jsx
+++ src/Students.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 const Students = () => {
 
   const [index, setIndex] = useState(0)
+  const [bioState, setBioState] = useState(false)
   let student = studentList[index]
 
 
@@ -11,6 +12,11 @@ const Students = () => {
     let content = evt.target.textContent
       content === 'Next' ? index === studentList.length - 1 ? setIndex(0) : setIndex(index + 1) :
       index === 0 ? setIndex(studentList.length - 1) : setIndex(index - 1)
+  }
+
+  function toggleBio(evt) {
+    setBioState(!bioState)
+    evt.target.textContent === 'Show Bio' ? evt.target.textContent = 'Hide Bio' : evt.target.textContent = 'Show Bio'
   }
 
   return (
@@ -22,7 +28,10 @@ const Students = () => {
         Previous
       </button>
       <h1>{student.name}</h1>
-      <h2>{student.bio}</h2>
+      <button onClick={toggleBio}>
+        Show Bio
+      </button>
+      {bioState && <h2>{student.bio}</h2>}
     </>
   )
 }
