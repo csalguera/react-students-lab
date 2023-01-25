e7838ce2bceeb13a9c72898eb2d7bbdcb6103fde
Carlos Salguera <carlos.e.salguera@gmail.com>
2023-01-24 20:39:16 -0500

    add styling

--- src/Score.jsx
+++ src/Score.jsx
@@ -2,8 +2,8 @@ const Score = ({ num, score }) => {
   return (
     <>
       <h3>Score {num + 1}:</h3>
-      <p>{score.date}</p>
-      <p>{score.score}</p>
+      <p>Date: {score.date}</p>
+      <p>Score: {score.score}</p>
     </>
   )
 }
